1699a455852d151d88dd340b890b02bbd1d00092
HadertS <source.zero@googlemail.com>
2023-04-27 11:29:57 +0100

    bugfix to timestamp converter, name search route added, additional verification moved to checkjsonMiddleware, Checkinservice removed as was redundant

--- Middleware/checkJsonMiddleware.js
+++ Middleware/checkJsonMiddleware.js
@@ -3,9 +3,13 @@ const jsonResponseService = require("../Services/jsonResponseService");
 const checkJsonMiddleware = (req, res, next) => {
     if(req.headers['content-type'] != 'application/json') {
         return res.json(jsonResponseService('Error: Only JSON content is permitted', [], 400))
-    } else {
-        next()
     }
+    if(Object.keys(req.body).length === 0) {
+        return res.json(jsonResponseService("Error: Request cannot be empty",[],500))
+    }
+
+    next()
+
 }
 
 module.exports = checkJsonMiddleware
